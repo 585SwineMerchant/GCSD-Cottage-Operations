1e0383bc447946117733639686401d68e888986c
Kevin McCann <kevinseanmccann78@gmail.com>
2026-09-19 15:20:09 -0400

    Connect GCSD Google deployments

--- site/config.js
+++ site/config.js
@@ -1,6 +1,5 @@
 window.GCSD_CONFIG = Object.freeze({
   // Add only deployment URLs created specifically for this version-two project.
-  publicFeedUrl: "",
-  teacherCommandCenterUrl: ""
+  publicFeedUrl: "https://script.google.com/macros/s/AKfycbw7G8tbAHboupxtWsWwNVQBg-pXp1_5pNl3_0AfqfaOAK3mg1kWIWqguU47Jp5XhRTdwg/exec",
+  teacherCommandCenterUrl: "https://script.google.com/a/macros/greececsd.org/s/AKfycbxPUz7-_Fijt3TIA0lmwcWyZgefE17OtWiDL7U98DWxD8x643tXFVDIsEuhuop7WYTc/exec"
 });
-
