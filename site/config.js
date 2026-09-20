da5acd492d7da9d5093e4f6022bb4d56f683fdcd
Kevin McCann <kevinseanmccann78@gmail.com>
2026-09-20 16:47:15 -0400

    Add structured recipe URL import

--- site/config.js
+++ site/config.js
@@ -1,5 +1,6 @@
 window.GCSD_CONFIG = Object.freeze({
   // Add only deployment URLs created specifically for this version-two project.
   publicFeedUrl: "https://script.google.com/macros/s/AKfycbw7G8tbAHboupxtWsWwNVQBg-pXp1_5pNl3_0AfqfaOAK3mg1kWIWqguU47Jp5XhRTdwg/exec",
-  teacherCommandCenterUrl: "https://script.google.com/a/macros/greececsd.org/s/AKfycbxPUz7-_Fijt3TIA0lmwcWyZgefE17OtWiDL7U98DWxD8x643tXFVDIsEuhuop7WYTc/exec"
+  teacherCommandCenterUrl: "https://script.google.com/a/macros/greececsd.org/s/AKfycbxPUz7-_Fijt3TIA0lmwcWyZgefE17OtWiDL7U98DWxD8x643tXFVDIsEuhuop7WYTc/exec",
+  recipeImportUrl: ""
 });
