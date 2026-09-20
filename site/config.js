e489900ab0062b3dcf0a150ab88bbcb63672a1b1
Kevin McCann <kevinseanmccann78@gmail.com>
2026-09-20 17:28:16 -0400

    Remove teacher access from public site

--- site/config.js
+++ site/config.js
@@ -1,6 +1,5 @@
 window.GCSD_CONFIG = Object.freeze({
   // Add only deployment URLs created specifically for this version-two project.
   publicFeedUrl: "https://script.google.com/macros/s/AKfycbw7G8tbAHboupxtWsWwNVQBg-pXp1_5pNl3_0AfqfaOAK3mg1kWIWqguU47Jp5XhRTdwg/exec",
-  teacherCommandCenterUrl: "https://script.google.com/a/macros/greececsd.org/s/AKfycbxPUz7-_Fijt3TIA0lmwcWyZgefE17OtWiDL7U98DWxD8x643tXFVDIsEuhuop7WYTc/exec",
   recipeImportUrl: "https://script.google.com/macros/s/AKfycbwwwyAlMHHyguX-lj2g0RFSm2dRnj3fw35SOHFeFj3RkVu52zmDO9jBND9OuwaMwqs8/exec"
 });
