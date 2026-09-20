f6ea7087fafd865ff38f361a9c15cd04c5e0b020
Kevin McCann <kevinseanmccann78@gmail.com>
2026-09-20 16:47:14 -0400

    Add structured recipe URL import

--- site/config.example.js
+++ site/config.example.js
@@ -1,5 +1,6 @@
 window.GCSD_CONFIG = Object.freeze({
-  // Use only the two /exec URLs created for GCSD Cottage Operations version two.
+  // Use only /exec URLs created for GCSD Cottage Operations version two.
   publicFeedUrl: "https://script.google.com/macros/s/PUBLIC_FEED_DEPLOYMENT_ID/exec",
-  teacherCommandCenterUrl: "https://script.google.com/macros/s/TEACHER_DEPLOYMENT_ID/exec"
+  teacherCommandCenterUrl: "https://script.google.com/macros/s/TEACHER_DEPLOYMENT_ID/exec",
+  recipeImportUrl: "https://script.google.com/macros/s/RECIPE_IMPORT_DEPLOYMENT_ID/exec"
 });
